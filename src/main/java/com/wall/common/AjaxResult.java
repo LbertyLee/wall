0a79a3e60c43a8a6fc07bb776a32ee37ab4a04ad
臧先生 <10868186+lihng@user.noreply.gitee.com>
2022-08-22 14:58:00 +0800

    li

--- src/main/java/com/wall/common/AjaxResult.java
+++ src/main/java/com/wall/common/AjaxResult.java
@@ -12,6 +12,7 @@ public class AjaxResult extends HashMap<String, Object>
 {
     private static final long serialVersionUID = 1L;
 
+
     /** 状态码 */
     public static final String CODE_TAG = "code";
 
